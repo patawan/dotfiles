2c047ef96a1c24ec04b8bff40ab0d73dc8fe7e30
Patrick H <hess.patrick@proton.me>
2025-01-07 14:49:33 -0700

    remove all

--- README.md
+++
@@ -1,2 +0,0 @@
-# dotfiles
-my dotfiles for linux and random stuff
--- fish/config.fish
+++
@@ -1,6 +0,0 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-    pyenv init - | source
-    oh-my-posh init fish --config /home/patrick/.config/oh-my-posh/themes/multiverse-neon-extra-slim.omp.json | source
-    fastfetch
-end
